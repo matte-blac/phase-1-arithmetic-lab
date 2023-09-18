95f833a2ef13c5fcf70494e9c211a5ae91f862cc
Matthew Bomett <bomettmat2@gmail.com>
2023-09-19 00:34:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+
+
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 16
+const num4 = 4
+let mod = num3 / num4;
+
+let max = Math.max(5, 10, 15, 20);
+
+console.log(multiply);
+console.log(random);
+console.log(mod);
+console.log(max);
